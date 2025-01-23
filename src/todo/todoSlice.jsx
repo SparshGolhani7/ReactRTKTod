2b6843355414bbdfd79873bfd9413a814bda4ce3
dev <dev@examplegmail.com>
2025-01-23 18:35:59 +0530

    review for PR

--- src/todo/todoSlice.jsx
+++ src/todo/todoSlice.jsx
@@ -1,9 +1,10 @@
 import {createSlice, nanoid} from '@reduxjs/toolkit'
 
 const initialState = {
-    todos:[{id:1,text:"First Task"}]
-    
+    todos:[{id:1,text:"First Task"}],
+    updateId:null,
 };
+
 export const todoSlice = createSlice({
     name:"todo",
     initialState,
@@ -15,23 +16,56 @@ export const todoSlice = createSlice({
             }
             state.todos.push(todo);
         },
+        
+
+        // updateTask:(state,action)=>{
+        //     state.todos.map((todo)=>{
+        //     // const updatedText = todo.text;
+        //    if(todo.id===action.payload.id){
+        //     console.log(action,"act");
+        //     state.updateId = todo.id
+        // //  todo.text =action.payload.text
+        // //   const updatedText = todo.text;
+        // // return {
+        // //     ...todo,
+        // //     text:action.payload.text,
+        // // };  
+        //  } 
+
+        
         updateTask:(state,action)=>{
-            state.todos.map((todo)=>{
-            // const updatedText = todo.text;
-           if(todo.id===action.payload.id){
-            console.log(action,"act");
-        //  todo.text =action.payload.text
-        //   const updatedText = todo.text;
-        return {
-            ...todo,
-            text:action.payload.text,
-        };  
-         } 
-         return todo                        
-            })
-        },
+            console.log("new",action);
+           const {id,text} = action.payload;
+
+           const taskToUpdate= state.todos.find((todo)=>todo.id===id)
+           if(taskToUpdate){
+            taskToUpdate.text =text;
+            console.log("jsj",taskToUpdate.text);
+           }
+            
+         } ,
+
+
+
+        //  return todo     
+            //     if(todo.id===state.updateId){
+            //         // todo.text = action.payload
+                    
+            //         console.log("finalUpdate",action.payload);
+            //     }      
+            //     return todo;   
+
+
+
+        //     })
+        // },
         removeTask:(state,action)=>{
-           state.todos = state.todos.filter((todo)=>todo.id !==action.payload)
+        //    state.todos = state.todos.filter((todo)=>todo.id !==action.payload)
+       return{
+        ...state,
+        todos:state.todos.filter((todo)=>todo.id !==action.payload)
+       } 
+
         },
     },
 
@@ -39,3 +73,5 @@ export const todoSlice = createSlice({
 
 export const {addTask,updateTask,removeTask} = todoSlice.actions
 export default todoSlice.reducer
+
+
